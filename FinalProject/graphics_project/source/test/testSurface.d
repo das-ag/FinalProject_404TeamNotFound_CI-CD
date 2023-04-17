b6e8dd3c926f7b1e60ffbfce559efbaee54ded10
das-ag <das.ag@northeastern.edu>
2023-04-16 20:01:18 -0400

    Added more tests

--- FinalProject/graphics_project/source/test/testSurface.d
+++ FinalProject/graphics_project/source/test/testSurface.d
@@ -8,6 +8,20 @@ import loader = bindbc.loader.sharedlib;
 import Surface : Surface;
 
 
+@("Test Get pixel")
+unittest{
+    const int width = 640;
+    const int height = 480;
+    const int btnHt = 50;
+    loadSDL();
+    SDL_Init(SDL_INIT_EVERYTHING);
+    
+    Surface imgSurface = Surface(width, height, btnHt);
+    auto p = imgSurface.getPixel(10,10);
+    assert(p[0]== 0 && p[1]== 0 && p[2]==0);
+    destroy(imgSurface);
+}
+
 @("Test Update pixel")
 unittest{
     const int width = 640;
@@ -19,7 +33,9 @@ unittest{
     Surface imgSurface = Surface(width, height, btnHt);
     imgSurface.UpdateSurfacePixel(10, 10, 30, 40, 50);
     auto p = imgSurface.getPixel(10,10);
-    writeln(p);
     assert(p[0]== 30 && p[1]== 40 && p[2]==50);
     destroy(imgSurface);
 }
+
+
+
--- FinalProject/graphics_project/source/test/testWindow.d
+++ FinalProject/graphics_project/source/test/testWindow.d
@@ -5,7 +5,7 @@ import loader = bindbc.loader.sharedlib;
 
 import Window : Window;
 
-@("Test getWindowSurface")
+@("Test Window dimensions")
 unittest{
     loadSDL();
     SDL_Init(SDL_INIT_EVERYTHING);
@@ -15,5 +15,18 @@ unittest{
 
     Window testWindow = Window("test window", width, height);
     assert(testWindow.width == width && testWindow.height == 480);
+    destroy(testWindow);
+}
+
+@("Test Window.getWindowSurface")
+unittest{
+    loadSDL();
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    const int width = 640;
+    const int height = 480;
+
+    Window testWindow = Window("test window", width, height);
+    assert(typeid(testWindow.getWindowSurface) == typeid(SDL_Surface*));
     destroy(testWindow);
 }
