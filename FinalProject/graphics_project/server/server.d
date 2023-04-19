70d7ef1e85b7288e2d983ad1bda26b34b856661d
Heekyung Kim <33532529+hkimkim@users.noreply.github.com>
2023-04-19 00:08:27 -0400

    Clean up server.d

--- FinalProject/graphics_project/server/server.d
+++ FinalProject/graphics_project/server/server.d
@@ -1,6 +1,7 @@
 /***
  * Class that runs a server that accepts multiple client connections.
  * A thread is spwaned for each client to broadcast packets.
+ * Authors: Agastya Das, Heekyung Kim, Roydon Pereira, Jake Stringfellow, Jiayue Zhao
  */
 module server;
 
@@ -10,12 +11,13 @@ import core.thread.osthread;
 import std.conv;
 import core.stdc.string;
 
+
 /**
 Function that retrieves local IP address of server
 Returns: local IP address of server as char array
 */
-char[] GetIP(){
-    auto r = getAddress("8.8.8.8",53); 
+char[] getIP(){
+    auto r = getAddress("8.8.8.8", 53); 
     auto sockfd = new Socket(AddressFamily.INET,  SocketType.STREAM);
    
     import std.conv;
@@ -45,9 +47,9 @@ class TCPServer{
 		port = port to open to communicate with clients (default is 50002)
 		maxConnectionsBacklog = number of pending connections the server queue will hold (default is 4)
 	*/
-	this(string host = "localhost", ushort port=50_002, ushort maxConnectionsBacklog=4){
+	this(string host="localhost", ushort port=50_002, ushort maxConnectionsBacklog=4){
 		
-		host = to!string(GetIP());
+		host = to!string(getIP());
 		writeln("Starting server at: ", host, ":", port);
 		writeln("Server must be started before clients may join");
 
@@ -73,19 +75,19 @@ class TCPServer{
 	and spawns new thread that broadcasts all received packets to clients
 	*/
 	void run(){
-		bool serverIsRunning=true;
-		while(serverIsRunning){
+		bool serverIsRunning = true;
+		while(serverIsRunning)
+		{
 
 			// The servers job now is to just accept connections
 			writeln("Waiting to accept more connections");
 
-			/// accept is a blocking call.
+			/// Accept is a blocking call.
 			auto newClientSocket = mListeningSocket.accept();
 
 			// After a new connection is accepted, let's confirm.
 			writeln("Hey, a new client joined!");
-			writeln("(me)",newClientSocket.localAddress(),"<---->",newClientSocket.remoteAddress(),"(client)");
-
+			writeln("(me)", newClientSocket.localAddress(), "<---->", newClientSocket.remoteAddress(), "(client)");
 
 			// Add newly joined client's socket to array of client sockets
 			mClientsConnectedToServer ~= newClientSocket;
@@ -93,7 +95,7 @@ class TCPServer{
 			// Set the current client to have '0' total messages received
 			mCurrentMessageToSend ~= 0;
 
-			writeln("Friends on server = ",mClientsConnectedToServer.length);
+			writeln("Friends on server = ", mClientsConnectedToServer.length);
 
 			// Spawn a new thread for each newly joined client
 			// that relay messages to clients.
@@ -111,22 +113,20 @@ class TCPServer{
 		clientSocket = Socket to listen and broadcast to client
 	*/
 	void clientLoop(Socket clientSocket){
-			writeln("\t Starting clientLoop:(me)",clientSocket.localAddress(),"<---->",clientSocket.remoteAddress(),"(client)");
+		writeln("\t Starting clientLoop:(me)",clientSocket.localAddress(),"<---->",clientSocket.remoteAddress(),"(client)");
 		
 		bool runThreadLoop = true;
 		byte[] dummyBuffer;
-		int nextBytes = 0;
 		int missingBytes = 0;
 
 		while(runThreadLoop){
 			// Check if the socket isAlive
 			if(!clientSocket.isAlive){
 				// Then remove the socket
-				runThreadLoop=false;
+				runThreadLoop = false;
 				break;
 			}
 
-			
 			byte[32] finalBuffer;
 			byte[32] receivedBuffer;
 
@@ -135,65 +135,43 @@ class TCPServer{
 
 			actualReceivedBytes = cast(int) clientSocket.receive(receivedBuffer);
 
-
             // Check if all the bytes are received from server
-            // if not save bytes to a dummy buffer until 
-            // all the bytes are received
+            // if not, save bytes to a dummy buffer until 
+            // all the bytes are received. 
+			// When all the bytes are received, then broadcast to all the clients
 			if (actualReceivedBytes > 0) {
 
-				writeln("first loop nextbytes: ", nextBytes);
-				writeln("first loop actualReceivedBytes: ", actualReceivedBytes);
-				writeln("receivedBytes: ", receivedBuffer);
-
-
 				if (missingBytes == 0 && actualReceivedBytes < expectedBytesToReceive) {
-					writeln("conditio 1-1");
 					dummyBuffer = receivedBuffer[0 .. actualReceivedBytes].dup;
 					missingBytes = expectedBytesToReceive - actualReceivedBytes;
-					writeln("dummy buffer:", dummyBuffer);
-					writeln("missing bytes: ", missingBytes);
 					continue;
 				}
 
 
 				if (missingBytes != 0) {
-					if (missingBytes < actualReceivedBytes) {
-						writeln("condition 2-1");
+					if (missingBytes < actualReceivedBytes) 
+					{
 						dummyBuffer ~= receivedBuffer[0 .. missingBytes].dup;
 						finalBuffer = dummyBuffer[0 .. expectedBytesToReceive].dup;
 						dummyBuffer = [];
 						dummyBuffer = receivedBuffer[missingBytes .. actualReceivedBytes].dup;
 						missingBytes = expectedBytesToReceive - cast(int)dummyBuffer.length;
-						writeln("dummy buffer:", dummyBuffer);
-						writeln("final: ", missingBytes);
-						writeln("missing: ", missingBytes);
-
 					}
-					else if (missingBytes == actualReceivedBytes) {
-						writeln("condition 2-2");
+					else if (missingBytes == actualReceivedBytes) 
+					{
 						dummyBuffer ~= receivedBuffer[0 .. missingBytes].dup;
 						finalBuffer = dummyBuffer[0 .. expectedBytesToReceive].dup;
 						dummyBuffer = [];
 						missingBytes = 0;
-						writeln("dummy buffer:", dummyBuffer);
-						writeln("final: ", missingBytes);
-						writeln("missing: ", missingBytes);
 					}
-					else if (missingBytes > actualReceivedBytes) {
-						writeln("condition 2-3");
+					else if (missingBytes > actualReceivedBytes) 
+					{
 						dummyBuffer ~= receivedBuffer[0 .. actualReceivedBytes].dup;
 						missingBytes = expectedBytesToReceive = cast(int)dummyBuffer.length;
-						writeln("dummy buffer:", dummyBuffer);
-						writeln("final: ", missingBytes);
-						writeln("missing: ", missingBytes);
 					}
 				}
 				else if (missingBytes == 0 && actualReceivedBytes == expectedBytesToReceive) {
 					finalBuffer = receivedBuffer[0 .. expectedBytesToReceive].dup;
-					writeln("condition 3!");
-					writeln("dummy buffer:", dummyBuffer);
-					writeln("final: ", missingBytes);
-					writeln("missing: ", missingBytes);
 				}
 			}
 
@@ -202,6 +180,7 @@ class TCPServer{
 
 				mServerData ~= finalBuffer;
 
+				// Check the final buffer sending to clients
 				writeln(finalBuffer);
 
 				/// After we receive a single packet, we'll just 
@@ -234,21 +213,14 @@ class TCPServer{
 		}
 	}
 
-	/// The listening socket is responsible for handling new client connections.
-	Socket 		mListeningSocket;
-	/// Stores the clients that are currently connected to the server.
-	Socket[] 	mClientsConnectedToServer;
-
-	/// Stores all of the data on the server. Ideally, we'll 
-	/// use this to broadcast out to clients connected.
-	byte[32][] mServerData;
-	/// Keeps track of the last message that was broadcast out to each client.
-	uint[] 			mCurrentMessageToSend;
+	Socket mListeningSocket;					/// The listening socket is responsible for handling new client connections.
+	Socket[] mClientsConnectedToServer;			/// Stores the clients that are currently connected to the server.
+	byte[32][] mServerData;						/// Stores all of the data on the server. 
+	uint[] mCurrentMessageToSend;				/// Keeps track of the last message that was broadcast out to each client.
 }
 
 // Entry point to Server
 void main(){
-
 	TCPServer server = new TCPServer;
 	server.run();
 }
